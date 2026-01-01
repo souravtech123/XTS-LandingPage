022a5e3ca0d5f07b05c104078e52f4b0bd08d417
Sourav Suman <souravsuman846@gmail.com>
2026-01-01 14:59:22 +0530

    Added Manifest

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+import { registerSW } from "virtual:pwa-register";
+registerSW({ immediate: true });
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <App />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +9,30 @@ export default defineConfig({
     react(),
     tailwindcss(),
 
-
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Xavier Tech Byte Society",
+        short_name: "XTB",
+        description: "Tech community app for events and opportunities",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",   // ✅ ONLY THIS
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",   // ✅ ONLY THIS
+          },
+        ],
+      },
+    }),
   ],
-})
+});
